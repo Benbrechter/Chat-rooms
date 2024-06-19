7b90b017e26edc3fdf3ce6f60ef7d4d250fc4bf2
Benjamin Brechter <benbrechter3@gmail.com>
2024-06-19 16:57:12 -0500

    I added the nesissary models

---
+++ server/models/message.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const messageSchema = new Schema({
+    messageText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 1000
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Message = mongoose.model('Message', messageSchema)
+
+module.exports = Message
